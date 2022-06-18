777454434a703cc235d39584d640e9b999a54416
Shakadeliks <shakurxmatiwana@gmail.com>
2022-06-18 13:11:54 +0200

    app deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeader: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeader: 'X-RapidAPI-Key',
+  XRapidAPIKeyValue: '022c043086mshe62f954ba4073f0p1f981ejsnf718ea969f96'
 };
